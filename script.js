eda24fad09acc34ecef2237f252f37461f0866bf
AndrewSimons3 <simdrew07@gmail.com>
2022-08-16 11:04:09 -0700

    finished js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 'use strict';
+const adviceBtn = document.getElementById('advice-btn');
+const adviceText = document.getElementById('advice-text');
+const adviceId = document.getElementById('advice-id');
 
-console.log('hello world');
+const fetchAdvice = async () => {
+	const response = await fetch('https://api.adviceslip.com/advice');
+	const data = await response.json();
+	return data;
+};
+
+const generateAdvice = async () => {
+	const advice = await fetchAdvice();
+	console.log(advice);
+
+	adviceText.innerHTML = `${advice.slip.advice}`;
+	adviceId.innerHTML = `${advice.slip.id}`;
+};
+
+adviceBtn.addEventListener('click', generateAdvice);
